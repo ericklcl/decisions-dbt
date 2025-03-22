0341a5ac7585baa67289abf1d9448c5cb496b3c9
ericklcl <ericklcl@gmail.com>
2025-03-22 15:07:59 -0300

    arquivos de materializacao incremental

---
+++ elima_dbt_project01/models/incremental/incremental_01.sql
@@ -0,0 +1,9 @@
+{{
+  config(
+    materialized = 'incremental'
+    )
+}}
+select * from {{ ref('seed_orders') }}
+{% if is_incremental() %}
+where last_updated > (select max(last_updated) from {{ this }})
+{% endif %}
